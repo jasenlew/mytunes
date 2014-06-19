cb06401cb1d7a68dc60331047a7fcdae763aa4cc
Hack Reactor Students <jasen.lew@gmail.com>
2014-06-19 12:06:40 -0700

    Implement playcount functionality that updates on plays

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,15 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  initialize: function () {
+    this.set('playcount', 0);
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+    this.set('playcount', this.get('playcount') + 1);
+//    console.log(this.get('playcount'));
   },
   enqueue: function(){
     this.trigger('enqueue', this);
--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -1,9 +1,17 @@
 // LibraryEntryView.js - Defines a backbone view class for the entries that will appear within the library views. These will be inserted using the "subview" pattern.
 var LibraryEntryView = Backbone.View.extend({
 
+  // Listens to a change on the model, including change in playcount.
+  // Then which, re-renders the library list with correct playcount.
+  initialize: function(){
+    this.model.on('change', function(){
+      this.render();
+    }, this);
+  },
+
   tagName: 'tr',
 
-  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td><td>(Plays: <%= playcount %>)</td>'),
 
   events: {
     'click': function() {
